fa705e682155801b79e67b5a8deb1bf841b6b82f
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-08-05 09:41:01 +0700

    Update RegistrationService.java

--- src/main/java/com/example/registrationapi/registration/RegistrationService.java
+++ src/main/java/com/example/registrationapi/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package com.example.registrationapi.registration;
 
+import com.example.registrationapi.appuser.AppUser;
+import com.example.registrationapi.appuser.AppUserRole;
+import com.example.registrationapi.appuser.AppUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class RegistrationService {
 
+    private final AppUserService appUserService;
     private final EmailValidator emailValidator;
 
     public String register(RegistrationRequest request) {
@@ -15,7 +19,11 @@ public class RegistrationService {
         if (!isValidEmail) {
             throw new IllegalStateException("email not valid");
         }
-
-        return "works";
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER));
     }
 }
