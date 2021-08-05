7a1d8a9745436b9359f3751a9188d1ae5d8e63cd
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-08-05 17:05:11 +0700

    added method for enable user

--- src/main/java/com/example/registrationapi/appuser/AppUserService.java
+++ src/main/java/com/example/registrationapi/appuser/AppUserService.java
@@ -53,4 +53,8 @@ public class AppUserService implements UserDetailsService {
         //TODO: Send Email
         return token;
     }
+
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
+    }
 }
