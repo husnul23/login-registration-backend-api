8df78879b451bcb5ec1f9df0c5744003feb0b958
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-08-06 10:43:10 +0700

    fix setFrom method value

--- src/main/java/com/example/registrationapi/email/EmailService.java
+++ src/main/java/com/example/registrationapi/email/EmailService.java
@@ -15,22 +15,26 @@ import javax.mail.internet.MimeMessage;
 @AllArgsConstructor
 public class EmailService implements EmailSender{
 
-    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
-    private final JavaMailSender javaMailSender;
+    private final static Logger LOGGER = LoggerFactory
+            .getLogger(EmailService.class);
+
+    private final JavaMailSender mailSender;
 
     @Override
     @Async
     public void send(String to, String email) {
         try {
-            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(mimeMessage, "utf-8");
             helper.setText(email, true);
             helper.setTo(to);
             helper.setSubject("Confirm your email");
-            helper.setFrom("Your email confirmed");
+            helper.setFrom("supriyono@gmail.com");
+            mailSender.send(mimeMessage);
         } catch (MessagingException e) {
-            LOGGER.error("Failed to send email", e);
-            throw new IllegalStateException("Failed to send email");
+            LOGGER.error("failed to send email", e);
+            throw new IllegalStateException("failed to send email");
         }
     }
 }
